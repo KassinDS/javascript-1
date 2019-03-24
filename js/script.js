6634a55276e3e5cb38458fdcb60689b945c31eac
KassinDS <kassinds@mail.ru>
2019-03-24 20:46:49 +0300

    первое домашнее задание

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var cT = + prompt("Введите температуру по Цельсию");
+var fT = 1.8 * cT + 32;
+console.log("Температура по Фарингейту: " + fT + " градусов.");
+
+
+var name = "Василий";
+var admin = name;
+console.log(admin);
+
+var x = 1000 + "108";
+console.log(x);
