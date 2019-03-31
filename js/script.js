9bfdd89159dd88298a1148c78b2719a67327d707
KassinDS <kassinds@mail.ru>
2019-03-31 09:55:23 +0300

    1. переделано задание 4 и 6 2. дописано задание 8

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Задание № 1
 
+/*
 var a = 1, b = 1, c, d;
 c = ++a; console.log (c);           // 2- сначала присваивает, потом выводит 
 d = b++; console.log (d);           // 1- сначала выводит, потом присваивает
@@ -7,17 +8,20 @@ c = (2+ ++a); console.log (c);      // (2 + 3) - 5
 d = (2+ b++); console.log (d);      // (2 + 2) - 4
 console.log (a);                    // 3
 console.log (b);                    // 3
+*/
 
 // Задание № 2
 
+/*
 var a = 2;
-var x = 1 + (a *= 2); // 5
+var x = 1 + (a *= 2); // 1 + (2 * 2) = 5
 console.log ('Результат задания №2 - ' + x);
+*/
 
 // Задание № 3
 
-var a = +prompt ('Введите число А');
-var b = +prompt ('Введите число Б');
+var a = +prompt ('Задание 3: Введите число А');
+var b = +prompt ('Задание 3: Введите число Б');
 
 if (a >= 0 && b >= 0) {
 	console.log ('Результат задания № 3 - ' + (a - b))
@@ -31,55 +35,39 @@ else if ((a >= 0 && b < 0) || (a < 0 && b >= 0)) {
 
 // Задание № 4
 
-var a = +prompt('Введите число от 0 до 15');
+console.log ('Результат заданя № 4: ')
+var a = +prompt('Задание 4: Введите число от 0 до 15');
 switch (a) {
-	case 0:
-	console.log('Результат задания № 4 - ' + ('1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15'));
-	break;
+case 0:
+	console.log(1);
 case 1:
-	console.log('Результат задания № 4 - ' + ('2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15'));
-	break;
+	console.log(2);
 case 2:
-	console.log('Результат задания № 4 - ' + ('3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15'));
-	break;
+	console.log(3);
 case 3:
-	console.log('Результат задания № 4 - ' + ('4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15'));
-	break;
+	console.log(4);
 case 4:
-	console.log('Результат задания № 4 - ' + ('5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15'));
-	break;
+	console.log(5);
 case 5:
-	console.log('Результат задания № 4 - ' + ('6, 7, 8, 9, 10, 11, 12, 13, 14, 15'));
-	break;
+	console.log(6);
 case 6:
-	console.log('Результат задания № 4 - ' + ('7, 8, 9, 10, 11, 12, 13, 14, 15'));
-	break;
+	console.log(7);
 case 7:
-	console.log('Результат задания № 4 - ' + ('8, 9, 10, 11, 12, 13, 14, 15'));
-	break;
+	console.log(8);
 case 8:
-	console.log('Результат задания № 4 - ' + ('9, 10, 11, 12, 13, 14, 15'));
-	break;
+	console.log(9);
 case 9:
-	console.log('Результат задания № 4 - ' + ('10, 11, 12, 13, 14, 15'));
-	break;
+	console.log(10);
 case 10:
-	console.log('Результат задания № 4 - ' + ('11, 12, 13, 14, 15'));
-	break;
+	console.log(11);
 case 11:
-	console.log('Результат задания № 4 - ' + ('12, 13, 14, 15'));
-	break;
+	console.log(12);
 case 12:
-	console.log('Результат задания № 4 - ' + ('13, 14, 15'));
-	break;
+	console.log(13);
 case 13:
-	console.log('Результат задания № 4 - ' + ('14, 15'));
-	break;
+	console.log(14);
 case 14:
-	console.log('Результат задания № 4 - ' + ('15'));
-	break;
-case 15:
-	console.log('Результат задания № 4 - ' + ('15'));
+	console.log(15);
 	break;
 default:
 	console.log('Результат задания № 4 - ' + 'Вы ввели неверное число')
@@ -87,28 +75,68 @@ default:
 
 // Задание № 5
 
-function addition (x, y)
-{
+/*
+var x = + prompt('Задание 5: введите число x');
+var y = + prompt('Задание 5: введите число y');
+*/
+
+function addition (x, y){
 	return (x + y);
 }
 
-function subtr (x, y)
-{
+function subtr (x, y){
 	return (x - y);
 }
 
-function multi (x, y)
-{
+function multi (x, y){
 	return (x * y);
 }
 
-function dividing (x, y)
-{
+function dividing (x, y){
 	return (x / y);
 }
 
-var add = addition(10, 10);
-var sub = subtr (50, 20);
-var mul = multi (2, 6);
-var div = dividing (10, 2);
-console.log ('Результат задания № 4 - ' + 'сложение - ' + add + ', вычитание - ' + sub + ', умножение - ' + mul + ', деление - ' + div);
+// Задание № 6
+
+var arg1 = + prompt('Задание 6: введите первое число');
+var arg2 = + prompt('Задание 6: введите второе число');
+var operation = + prompt('Задание 6: введите операцию: 1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление');
+
+function mathOperation (arg1, arg2, operation) {
+	switch(operation) {
+		case 1:
+			return addition (arg1, arg2);
+			break;
+		case 2:
+			return subtr (arg1, arg2);
+			break;
+		case 3:
+			return multi (arg1, arg2);
+			break;
+		case 4:
+			return dividing (arg1, arg2);
+			break;
+		default:
+			console.log('Вы ввели неверную операцию');
+	}
+}
+
+var add6 = mathOperation (arg1, arg2, operation);
+console.log('Результат №6 - ' + add6);
+
+// Задание № 8
+
+var val = +prompt('Задание 8: Введите число');
+var pow = +prompt('Задание 8: Введите степень')
+
+function power (val, pow) {
+	if (pow != 1) {
+		return val *= power(val, pow - 1)
+	} else {
+		return val;
+	}
+}
+
+var funcPow = power (val, pow);
+console.log(funcPow)
+
